15b447827d7e066da481dbb5a369adcc5f1a0b02
Artem <uspenskiyart@gmail.com>
2020-04-14 21:25:12 +0300

    feat: create main page with JS

--- script.js
+++ script.js
@@ -426,6 +426,14 @@ document.addEventListener('click', (event) =>{
 function mainPage() {
   const NAMES = ['Action (set A)', 'Action (set B)','Action (set C)','Adjective', 'Animal (set A)', 'Animal (set B)', 'Clothes', 'Emotions'];
   const IMAGES = ['data/img/fish.jpg','data/img/open.jpg','data/img/pull.jpg','data/img/old.jpg' ,'data/img/cat.jpg','data/img/lion.jpg','data/img/boot.jpg','data/img/laugh.jpg'];
+  const ID_SECTIONS = ["action_a","action_b","action_c","adjective","animal_a","animal_b","clothes","emotion"];
+  let z = 0;
+  for (let i = 0; i < NAMES.length; i++) {
+    document.querySelector('.main').insertAdjacentHTML('beforeend',`<section class="main_section_block" id=${ID_SECTIONS[z++]}>
+      <div class="section_img"></div>
+      <div class="section_description"></div>
+    </section>`)
+  }
   let descriptionOfMain = document.querySelectorAll('.section_description');
   let sectionImage = document.querySelectorAll('.section_img');
   let i=0;
@@ -443,5 +451,5 @@ mainPage();
 
 
 function createPage(arrayOfObjects) {
-  
+
 }
